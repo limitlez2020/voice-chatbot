c33e40f0bd10a332f02182657c96caf8d2959a49
fortune.akinremi <fortuneakinremi@gmail.com>
2024-11-29 21:56:07 -0500

    Fixed dependeciy issues

---
+++ app/components/SpeechToText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+
+export default function SpeechToText() {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
